d5807903fd55cadf8745b6c1e648e2c035a799bf
danish.khan <danish.khan@appinventiv.com>
2022-12-14 17:02:03 +0530

    fixes

--- app/Models/Common/Item.php
+++ app/Models/Common/Item.php
@@ -27,7 +27,7 @@ class Item extends Model
      *
      * @var array
      */
-    protected $fillable = ['company_id', 'name', 'description', 'sale_price', 'purchase_price', 'category_id', 'enabled', 'created_from', 'created_by'];
+    protected $fillable = ['company_id', 'name', 'description', 'sale_price', 'purchase_price', 'category_id', 'enabled', 'created_from', 'created_by','designation','reference_number'];
 
     /**
      * The attributes that should be cast.
